9adf88d64e19761b75e23084e3416f196aa9d9e9
jadelui <34103207+jadelui@users.noreply.github.com>
2017-11-30 15:57:35 +0100

    Update EEx36.py

--- EEx36.py
+++ EEx36.py
@@ -8,11 +8,10 @@ print """
 	You open your eyes. You are in a garden, trees, flowers, animals all around you.
     There is an apple tree beside you.
     You hear a voice from God:
-    ‘You are my creation, I love you, you can do whatever you want here’
-    ‘but don’t eat the apple from the tree’
-     You yell back:
-
-
+    'You are my creation, I love you, you can do whatever you want here'.
+    'but don't eat the apple from the tree'.
+    'What's your name?'
+    You yell back:
 	
 """
 
@@ -23,15 +22,14 @@ print "	'Thank you! I love you too %s!'" % name
 def garden_start():	
 	print """
 	You are walking in the garden.
-    You see a snake.
-    He said the apple on the tree is really delicious.
-
+	You see a snake.
+	He said the apple on the tree is really delicious.
 	
 	What do you do?
 	
 	1. Chat with the snake.
-	2. Ignore the snake.
-	3. Try the apple.
+	2. Ingnor the snake.
+	3. Try the apple
 	4. Not eating the apple and kill the snake.
 	
 	"""
@@ -42,13 +40,13 @@ def garden_start():
 	elif choice == 2:
 		go_away()
 	elif choice == 3:
-		eatting_apple()
+		eatting_apple("Live happily in the garden")
 	elif choice == 4:
-		won("Live happily in the garden.")
+		won("Live happily in the garden")
 	else:
 		dead("Confused, you die")	
 
-# details for Try the apple
+# details for eatting apple
 def eatting_apple():
 	print """
 	You start feeling guilty.
@@ -69,30 +67,24 @@ def eatting_apple():
 		dead("You are bited by a snake. You are dead.")
 	elif choice == 2:
 		print "You yell at God : Where are you!"
-		intelligence = random.randint(1,6)
-		print "God say you did something wrong %s." % intelligence
-		
-		if intelligence > 3:
-			print "You say sorry to God."
-			print "Your relationship with God is being better"
-			stables()
-		else:
-			dead("You deny it is your fault. You run out of the garden, you die of starvation.")
+		print "God say you did something wrong"
+		stables()
+
 	elif choice == 3:
 		garden_start()
 	else:
 		dead("Confused, you kill yourself somehow.")
 
-# details for guard room, first left
+# details for ignor.
 def go_away():
 	print """
 	You leave the garden.
-	You saw a sinful world.
+	You saw a prosperous world.
 	
 	What do you do?
 	
-	1. Go into the sinful world
-	2. Cry 
+	1. Walk into the world.
+	2. You desperate to join this world.
 	3. Turn back, quickly!
 	
 	"""
@@ -100,35 +92,27 @@ def go_away():
 	choice = int(raw_input(prompt))
 	
 	if choice == 1:
-		print "You saw people telling lies, being greedy, teasting others"
-		strength = random.randint(1,6)
-		print "	You do not know how to deal with these people %s." % strength
-		if strength > 3:
-			print """
-			You think you need to stop people from doing it.
+		print """	You saw people telling lies, being greedy, teasting others
+		            You think you need to stop people from doing it.
+		"""
+		stop_people()
 
-			"""
-			stop_people()
-		else:
-			dead("	You enjoy the sinful life with others, got punished by God")
 	elif choice == 2:
-		dead("The snake bited you. You died")
-	elif choice == 3:
-		garden_start()
-	else:
-		dead("Confused, you die.")
+		stables("You enjoy the sinful life with others, feel confused finally")
 	
-# details for stop people
+	else:
+		dead("The snake bited you. You died")
+	
+# details for boat room
 def stop_people():
 	print """
 	You stop people from worshiping other God.
 	You warn people to behave.
-	
+
 	What do you do?
 	
 	1. You pray for the sinful world.
-	2. You get used to the sinful world .
-
+	2. You get used to the sinful world.
 	
 	"""
 	
@@ -137,22 +121,20 @@ def stop_people():
 	if choice == 1:
 		won("You keep yourself away from sin.")
 	elif choice == 2:
-		dead("Someone rob and killed you")
-	
+		dead("Someone rob and kill you")
 	else:
-		dead("Confused, you fall into a lake and drown.")
+		dead("Confused, you fall into the water and drown.")
 
 # details for stables
 def stables():
 	print """
-	God said He loves you a lot 
+	God said He loves you a lot.
 	but you need to listen what he said sometimes
-	
+
 	What do you do?
 	
-	1. You love him as well, you agree.
-	2. You walk away from God.
-	3. Turn back.
+	1. You say sorry to God, and you love him as well.
+	2. You deny your sin, walk away for God.
 	
 	"""
 	
@@ -162,44 +144,37 @@ def stables():
 		won("You have a happy life with God after all.")
 	elif choice == 2:
 		dead("You dead of starvation.")
-	elif choice == 3:
-		dead("The snake bite you.")
+	
 	else:
-		dead("Confused, you are kicked in the head by a horse and killed.")
+		dead("Confused, you are bited by the snake")
 
-# details for dark room
+# details for chat with the snake
 def chatting_with_the_snake():
 	print """
-	You ask the snake why do this to you.
-	He said you can have all the properties in the world
+	You ask the snake why it does this to you.
+	He said you can have all the properties in the world.
 	
-	What do you do?
+	What you do?
 	
-	1. You killed the snake.
-	2. You eat the apple.
-	3. Turn back.
+	1. You eat the apple.
+	2. You killed the snake.
 	
 	"""
 	choice = int(raw_input(prompt))
 	
 	if choice == 1:
-		won("You keep yourself away from sin.")
+		eatting_apple()
 	elif choice == 2:
-		stables()
-	elif choice == 3:
-		garden_start()
-	else:
-		dead("Confused, you fall into a lake and drown.")
-	
+		won("You keep away from sin.")
 
 # details for dead function
 def dead(why):
-	print why, "Separate from God forever %s." % name
+	print why, "Separated from God forever %s." % name
 	exit(0)
 	
-# details for escape
+# details for won
 def won(why):
-	print why, "Congratulations %s!" % name
+	print why, "God loves you %s!" % name
 	exit(1)
 	
 garden_start()
